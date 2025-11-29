5e6a750ba8030a21e5b957759ec2d8e7dd572d3b
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2025-11-30 01:59:50 +0600

    suc

--- src/componets/about/Success.jsx
+++ src/componets/about/Success.jsx
@@ -10,7 +10,26 @@ const Success = () => {
                         <div className="">
                             <h2 className=' text-[96px] text-[#07152C] font-bold font-primary leading-[100%] ' >Success stories</h2>
                             <h3 className=' text-[96px] text-[#0055FF] font-bold font-primary leading-[100%] text-center'>That empower.</h3>
-                            <p className='text-[16px] text-[#07152C] font-normal font-primary leading-[150%] mt-[56px] w-[930px] '>Empowering businesses to chart their own course to success - that's our mission. We provide the tools, guidance, and unwavering support to help you navigate challenges, crush milestones, and turn your unique goals into resounding victories. Seeing our clients stand tall, empowered and thriving - that's the ultimate measure of our success.</p>
+                            <p className='text-[21px] text-[#07152C] font-normal font-primary leading-[150%] mt-[56px] w-[930px] '>Empowering businesses to chart their own course to success - that's our mission. We provide the tools, guidance, and unwavering support to help you navigate challenges, crush milestones, and turn your unique goals into resounding victories. Seeing our clients stand tall, empowered and thriving - that's the ultimate measure of our success.</p>
+                        </div>
+                        <div className=" flex justify-between gap-10 mt-[108px]">
+                            <div className="p-6 bg-[#E6EEFF]">
+                                <div className="">
+                                    <h2 className='text-[64px] text-[#2F3339] font-medium font-primary leading-[100%] '>500+</h2>
+                                    <h3 className='text-[30px] text-[#2F3339] font-normal font-primary leading-[100%] mt-4'>Businesses Thrived</h3>
+                                    <p className=' relative text-[24px] text-[#2F3339] font-normal font-primary  pl-[28px] w-[490px] mt-[60px] break-before-left before:absolute before:left-0 before:top-0 before:w-[5px] before:h-full before:bg-[#0055FF] before:rounded-full'>We helped more than 250 business to reach to their business goals with our innovative solutions.</p>
+                                </div>
+                            </div>
+                            <div className="p-6 bg-[#E6EEFF]">
+                                <div className="">
+                                    <h2 className='text-[64px] text-[#2F3339] font-medium font-primary leading-[100%] '>$2B+</h2>
+                                    <h3 className='text-[30px] text-[#2F3339] font-normal font-primary leading-[100%] mt-4'>Accumulated over $2B</h3>
+                                    <p className=' relative text-[24px] text-[#2F3339] font-normal font-primary  pl-[28px] w-[490px] mt-[60px] break-before-left before:absolute before:left-0 before:top-0 before:w-[5px] before:h-full before:bg-[#0055FF] before:rounded-full'>Working with businesses of all sizes, we've built a legacy of impact, exceeding $2+ billion, one life at a time.</p>
+                                </div>
+                            </div>
+                            
+                          
+                            
                         </div>
                     </div>
         </Container>
